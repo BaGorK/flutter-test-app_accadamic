3c9988e4fc4cb6b10566128e909c2cccb44e14dc
BaGorK <edmealemkassahun@gmail.com>
2024-04-05 10:44:04 +0300

    feat: list updated

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -49,6 +49,72 @@ class ListViewPage extends StatelessWidget {
           leading: const Icon(Icons.food_bank),
           trailing: const Icon(Icons.menu),
         ),
+        ListTile(
+          title: const Text("big burger on a wooden table with pepper"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Hawaiian Chicken PizzaSmoked"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Avocado and Egg Toast"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Meal with salmon and zucchini"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Vegan salad bowl"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Hawaiian Chicken PizzaSmoked"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Avocado and Egg Toast"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Meal with salmon and zucchini"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Vegan salad bowl"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("Homemade Burger"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
+        ListTile(
+          title: const Text("I scream You Scream"),
+          onTap: () {},
+          leading: const Icon(Icons.food_bank),
+          trailing: const Icon(Icons.menu),
+        ),
       ],
     );
   }
